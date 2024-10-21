94096b1cf98dae0ac3d96e9be1fd7f345337790e
arim83 <jorammercado@pursuit.org>
2024-10-21 14:37:20 -0400

    add login attempts queries

---
+++ queries/loginAttempts.js
@@ -0,0 +1,45 @@
+const db = require("../db/dbConfig.js")
+
+const createLoginAttempt = async (user_id, ip_address, success, device_fingerprint) => {
+    try {
+        const newLoginAttempt = await db.one(
+            `INSERT INTO login_attempts (user_id, ip_address, success, device_fingerprint) ` +
+            `VALUES ($1, $2, $3, $4) RETURNING *`,
+            [user_id, ip_address, success, device_fingerprint]
+        )
+        return newLoginAttempt
+    } catch (err) {
+        return { err: `${err}, sql query error - add login attempt` }
+    }
+}
+
+
+const getLoginAttemptsByUserId = async (user_id) => {
+    try {
+        const loginAttempts = await db.any(
+            `SELECT * FROM login_attempts WHERE user_id=$1 ORDER BY attempt_time DESC`,
+            [user_id]
+        )
+        return loginAttempts
+    } catch (err) {
+        return { err: `${err}, sql query error - get login attempts by user_id` }
+    }
+}
+
+
+const getAllLoginAttempts = async () => {
+    try {
+        const allLoginAttempts = await db.any(
+            `SELECT * FROM login_attempts ORDER BY attempt_time DESC`
+        )
+        return allLoginAttempts
+    } catch (err) {
+        return { err: `${err}, sql query error - get all login attempts` }
+    }
+}
+
+module.exports = {
+    createLoginAttempt,
+    getLoginAttemptsByUserId,
+    getAllLoginAttempts
+}
